28537f81932c87e99e6ae99bf65a5cc3d98b6549
Jake Wharton <jw@squareup.com>
2013-03-10 19:48:51 -0700

    Reduce the retention of the annotation to compile-time only.

--- butterknife/src/main/java/butterknife/InjectView.java
+++ butterknife/src/main/java/butterknife/InjectView.java
@@ -4,9 +4,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 import static java.lang.annotation.ElementType.FIELD;
-import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import static java.lang.annotation.RetentionPolicy.CLASS;
 
-@Retention(RUNTIME) @Target(FIELD)
+@Retention(CLASS) @Target(FIELD)
 public @interface InjectView {
   int value();
 }
