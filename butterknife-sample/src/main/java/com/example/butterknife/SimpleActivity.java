3cb069b214c8299d02a7025c538dff2c672da63b
Jake Wharton <jw@squareup.com>
2013-03-24 22:34:42 -0700

    Revert "Add ProGuard section to README."
    This reverts commit a7fb85dfcd361c27be562baffa9b8612cd761f5a.

--- butterknife-sample/src/main/java/com/example/butterknife/SimpleActivity.java
+++ butterknife-sample/src/main/java/com/example/butterknife/SimpleActivity.java
@@ -8,10 +8,9 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 import butterknife.InjectView;
-import butterknife.OnClick;
-import butterknife.OnLongClick;
 import butterknife.Views;
 
+import static android.view.View.OnClickListener;
 import static android.widget.Toast.LENGTH_SHORT;
 
 public class SimpleActivity extends Activity {
@@ -31,22 +30,11 @@ public class SimpleActivity extends Activity {
     subtitle.setText("View \"injection\" for Android.");
     footer.setText("by Jake Wharton");
     hello.setText("Say Hello");
+    hello.setOnClickListener(new OnClickListener() {
+      @Override public void onClick(View v) {
+        Toast.makeText(SimpleActivity.this, "Hello, views!", LENGTH_SHORT).show();
+      }
+    });
     listOfThings.setAdapter(new SimpleAdapter(this));
-  }
-
-  @OnClick(R.id.hello)
-  public void helloClicked(Button button) {
-    Toast.makeText(SimpleActivity.this, "Hello, click!", LENGTH_SHORT).show();
-    button.setText("Clicked!");
-  }
-
-  @OnClick({ R.id.title, R.id.subtitle, R.id.footer })
-  public void textClicked(TextView view) {
-    Toast.makeText(SimpleActivity.this, "Hello, views!", LENGTH_SHORT).show();
-  }
-
-  @OnLongClick(R.id.hello)
-  public void helloLongClicked(View view) {
-    Toast.makeText(SimpleActivity.this, "Hello, long click!", LENGTH_SHORT).show();
   }
 }
