4bd0ce9b1518bf1205928baeeebe9c65da8477ee
Vladimir Mironov <nsk.mironov@gmail.com>
2014-02-02 14:56:03 +0700

    Add documentation and usage example for @OnTextChanged

--- butterknife/src/main/java/butterknife/OnTextChanged.java
+++ butterknife/src/main/java/butterknife/OnTextChanged.java
@@ -8,6 +8,20 @@ import java.lang.annotation.Target;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.RetentionPolicy.CLASS;
 
+/**
+ * Annotation for methods which indicate that they should be called when a text view's input
+ * changed.
+ * Corresponds to adding a {@link android.text.TextWatcher} to the views specified by
+ * {@link #value()}.
+ * <pre><code>
+ * {@literal @}OnTextChanged(R.id.example) void onTextChanged(Editable text) {
+ *   Toast.makeText(this, "Text changed: " + text, LENGTH_SHORT).show();
+ * }
+ * </code></pre>
+ *
+ * @see android.text.TextWatcher
+ * @see Optional
+ */
 @Retention(CLASS) @Target(METHOD)
 @ListenerClass(
     name = "butterknife.internal.SimpleTextWatcher",
