dab494e1eeb5d47b9cc4f307b76d74f3878cb8a3
idosu <idobez@gmail.com>
2016-08-01 00:19:59 +0300

    Added ButterActivity and ButterFragment that helps with boiler plate code(calling setContentView and ButterKnife.bind)

---
+++ butterknife/src/main/java/butterknife/butterui/ButterActivity.java
@@ -0,0 +1,51 @@
+package butterknife.butterui;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import butterknife.ButterKnife;
+
+/**
+ * Extenders of this class can remove another part of the unneeded noise in the class.<br>
+ * Before:
+ * <pre class="code">
+ * public class MainActivity extends {@link Activity} {
+ *   &#064;{@link butterknife.BindView}(R.id.title) protected TextView title;
+ *
+ *   &#064;Override
+ *   public void onCreate(Bundle savedInstanceState) {
+ *     super.onCreate(savedInstanceState);
+ *     setContentView(R.layout.activity_main);
+ *     utterKnife.bind(this);
+ *   }
+ * }
+ * </pre>
+ * After:
+ * <pre class="code">
+ * &#064;{@link BindLayout}(R.layout.activity_main)
+ * public class MainActivity extends ButterActivity {
+ *   &#064;{@link butterknife.BindView}(R.id.title) protected TextView title;
+ * }
+ * </pre>
+ * @author
+ *      <br>Jul 31 2016 idosu
+ */
+public abstract class ButterActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        bind();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        bind();
+    }
+
+    private void bind() {
+        setContentView(BindLayoutUtil.getBindLayout(getClass()));
+        ButterKnife.bind(this);
+    }
+}
