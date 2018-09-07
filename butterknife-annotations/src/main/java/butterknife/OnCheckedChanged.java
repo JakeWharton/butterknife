31f940d5d1d8818e15fc5bfaec6056f396086b39
Jake Wharton <jakew@google.com>
2018-09-07 12:34:04 -0400

    Add checked change, editor action, focus, and item long click to reflect

--- butterknife-annotations/src/main/java/butterknife/OnCheckedChanged.java
+++ butterknife-annotations/src/main/java/butterknife/OnCheckedChanged.java
@@ -9,7 +9,7 @@ import java.lang.annotation.Target;
 
 import static android.widget.CompoundButton.OnCheckedChangeListener;
 import static java.lang.annotation.ElementType.METHOD;
-import static java.lang.annotation.RetentionPolicy.CLASS;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
  * Bind a method to an {@link OnCheckedChangeListener OnCheckedChangeListener} on the view for
@@ -26,7 +26,7 @@ import static java.lang.annotation.RetentionPolicy.CLASS;
  * @see OnCheckedChangeListener
  */
 @Target(METHOD)
-@Retention(CLASS)
+@Retention(RUNTIME)
 @ListenerClass(
     targetType = "android.widget.CompoundButton",
     setter = "setOnCheckedChangeListener",
--- butterknife-annotations/src/main/java/butterknife/OnItemLongClick.java
+++ butterknife-annotations/src/main/java/butterknife/OnItemLongClick.java
@@ -9,7 +9,7 @@ import java.lang.annotation.Target;
 
 import static android.widget.AdapterView.OnItemLongClickListener;
 import static java.lang.annotation.ElementType.METHOD;
-import static java.lang.annotation.RetentionPolicy.CLASS;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
  * Bind a method to an {@link OnItemLongClickListener OnItemLongClickListener} on the view for each
@@ -27,7 +27,7 @@ import static java.lang.annotation.RetentionPolicy.CLASS;
  * @see OnItemLongClickListener
  */
 @Target(METHOD)
-@Retention(CLASS)
+@Retention(RUNTIME)
 @ListenerClass(
     targetType = "android.widget.AdapterView<?>",
     setter = "setOnItemLongClickListener",
