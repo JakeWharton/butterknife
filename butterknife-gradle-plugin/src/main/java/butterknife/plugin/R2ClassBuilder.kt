492061c5c6699caa99b6e6167cdbc56a554286d7
Zac Sweers <pandanomic@gmail.com>
2019-02-07 18:40:55 -0800

    Create new R2ClassBuilder interface

---
+++ butterknife-gradle-plugin/src/main/java/butterknife/plugin/R2ClassBuilder.kt
@@ -0,0 +1,59 @@
+package butterknife.plugin
+
+import java.io.File
+
+/**
+ * High level interface for an "R2" class generator.
+ */
+internal interface R2ClassBuilder {
+  /**
+   * Adds a new resource field to render.
+   *
+   * @param type the [ResourceType].
+   * @param fieldName the resource field name.
+   * @param fieldValue the constant resource field value.
+   */
+  fun addResourceField(type: ResourceType, fieldName: String, fieldValue: String)
+
+  /**
+   * Writes the created class to the given output directory. Package structure will be created as
+   * needed.
+   *
+   * @property packageName the package name to write the RClass too
+   * @property className the simple class name to use for the RClass.
+   * @param outputDir the output directory.
+   */
+  fun write(
+      packageName: String,
+      className: String,
+      outputDir: File)
+
+  companion object {
+    /** The androidx annotations package. */
+    const val ANNOTATION_PACKAGE = "androidx.annotation"
+
+    /** Supported resource types. */
+    enum class ResourceType(val renderString: String) {
+      ANIM("anim"),
+      ARRAY("array"),
+      ATTR("attr"),
+      BOOL("bool"),
+      COLOR("color"),
+      DIMEN("dimen"),
+      DRAWABLE("drawable"),
+      ID("id"),
+      INTEGER("integer"),
+      LAYOUT("layout"),
+      MENU("menu"),
+      PLURALS("plurals"),
+      STRING("string"),
+      STYLE("style"),
+      STYLEABLE("styleable");
+
+      companion object {
+        /** Mapping of all the renderStrings to [ResourceType] for lookups. */
+        val RENDER_MAPPING = values().associate { it.renderString to it }
+      }
+    }
+  }
+}
