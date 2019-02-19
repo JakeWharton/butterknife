f55caf89ba70f5c1a63f25a9dc14830f521c35d4
D. Strekha <mr.choo96@gmail.com>
2019-02-19 15:09:21 +0300

    Add more doc

--- butterknife-annotations/src/main/java/butterknife/BindAttr.java
+++ butterknife-annotations/src/main/java/butterknife/BindAttr.java
@@ -10,8 +10,16 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
  * Bind a field to the specified attribute ID.
+ * Now it supports only colors presented as {@code int} (annotated with {@link androidx.annotation.ColorInt})
+ * and {@link android.content.res.ColorStateList}:
+ * <pre><code>
+ * {@literal @}BindColor(R.attr.colorAccent)
+ * {@literal @}ColorInt
+ * int colorAccent;
+ * {@literal @}BindColor(R.attr.someTextColor)
+ * ColorStateList textColor;
+ * </code></pre>
  */
-
 @Target(FIELD)
 @Retention(RUNTIME)
 public @interface BindAttr {
